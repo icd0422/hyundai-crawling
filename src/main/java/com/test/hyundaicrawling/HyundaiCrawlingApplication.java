dc514916461ea89ae22f1158fc254f558f2063b1
cori <cori.jang@bunjang.co.kr>
2023-02-27 16:29:08 +0900

    first init

---
+++ src/main/java/com/test/hyundaicrawling/HyundaiCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.test.hyundaicrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyundaiCrawlingApplication.class, args);
+    }
+        
+}
---
+++ src/test/java/com/test/hyundaicrawling/HyundaiCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.hyundaicrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundaiCrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
